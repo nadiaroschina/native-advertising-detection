a5c0c73fdc906e3fcd3bcb664d89e88712475ab2
yaninalex2003 <yanin.alex2003@yandex.ru>
2024-04-12 15:47:50 +0300

    add model in superweb.py

--- superweb.py
+++ superweb.py
@@ -1,17 +1,42 @@
 import streamlit as st
-import numpy as np
+import pandas as pd
+from catboost import CatBoostClassifier
+import pickle
 
-placeholder = st.empty()
+import sys
+sys.path.append('data')
+from data_preprocessor import DataPreprocessor
 
-with placeholder.form("Формочка"):
-    st.title("Поиск нативной рекламы")
-    ta = st.text_area("Введите текст:", height=240)
-    button = st.form_submit_button(label="Отправить", help=None, on_click=None)
-    if button:
-        st.info(f"Boom! Ваш текст: {ta}")
-        res = np.random.randint(2)
-        if res == 1:
-            st.warning('Скорее всего, это реклама', icon="⚠️")
-        else:
-            st.success('Не является рекламой', icon="✅")
-    
+vectorizer = pickle.load(open(("vectorizer.pickle"), "rb"))
+scaler = pickle.load(open(("scaler.pickle"), "rb"))
+model = CatBoostClassifier()
+model.load_model("catboost")
+preprocessor = DataPreprocessor()
+
+def vectorize(X):
+    X = preprocessor.fit_transform(X)
+    bow = vectorizer.transform(X)
+    return scaler.transform(bow)
+
+
+def predict(message_text: str) -> bool:
+    new_data = vectorize(pd.Series([message_text]))
+    return model.predict(new_data)[0] == 1
+
+def main() -> None:
+    placeholder = st.empty()
+
+    with placeholder.form("Формочка"):
+        st.title("Поиск нативной рекламы")
+        ta = st.text_area("Введите текст:", height=240)
+        button = st.form_submit_button(label="Отправить", help=None, on_click=None)
+        if button:
+            st.info(f"Boom! Ваш текст: {ta}")
+            res = predict(ta)
+            if res == 1:
+                st.warning('Скорее всего, это реклама', icon="⚠️")
+            else:
+                st.success('Не является рекламой', icon="✅")
+
+if __name__ == "__main__":
+    main()
